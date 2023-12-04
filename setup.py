ed42f648fbbd8cd286d9da6c5e7e1e215812b8eb
d116626 <d116626@gmail.com>
2023-12-04 15:34:46 -0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+'''
+Setup script for the package.
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name="prefeitura_rio",
+    version="1.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+
+)
