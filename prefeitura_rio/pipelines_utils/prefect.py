38a46d9220fdcf6024b0c130ca37e8fce9bde672
d116626 <d116626@gmail.com>
2023-12-15 18:02:05 -0300

    feat: add prefect pipelines_utils functions

--- prefeitura_rio/pipelines_utils/prefect.py
+++ prefeitura_rio/pipelines_utils/prefect.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from typing import List
+from datetime import datetime, timedelta
+from typing import List, Union
 
 try:
     import prefect
     from prefect import task
     from prefect.client import Client
+    from prefect.schedules.clocks import IntervalClock
 except ImportError:
     from prefeitura_rio.utils import base_assert_dependencies
 
     base_assert_dependencies(["prefect"], extras=["pipelines"])
+
+from prefeitura_rio.pipelines_utils.io import query_to_line
 
 
 def get_flow_run_mode() -> str:
@@ -111,3 +115,65 @@ def task_rename_current_flow_run_dataset_table(prefix: str, dataset_id: str, tab
     flow_run_id = prefect.context.get("flow_run_id")
     client = Client()
     return client.set_flow_run_name(flow_run_id, f"{prefix}{dataset_id}.{table_id}")
+
+
+def untuple_clocks(clocks):
+    """
+    Converts a list of tuples to a list of clocks.
+    """
+    return [clock[0] if isinstance(clock, tuple) else clock for clock in clocks]
+
+
+def generate_dump_db_schedules(  # pylint: disable=too-many-arguments,too-many-locals
+    interval: timedelta,
+    start_date: datetime,
+    labels: List[str],
+    db_database: str,
+    db_host: str,
+    db_port: Union[str, int],
+    db_type: str,
+    dataset_id: str,
+    vault_secret_path: str,
+    table_parameters: dict,
+    batch_size: int = 50000,
+    runs_interval_minutes: int = 15,
+) -> List[IntervalClock]:
+    """
+    Generates multiple schedules for database dumping.
+    """
+    db_port = str(db_port)
+    clocks = []
+    for count, (table_id, parameters) in enumerate(table_parameters.items()):
+        parameter_defaults = {
+            "batch_size": batch_size,
+            "vault_secret_path": vault_secret_path,
+            "db_database": db_database,
+            "db_host": db_host,
+            "db_port": db_port,
+            "db_type": db_type,
+            "dataset_id": dataset_id,
+            "table_id": table_id,
+            "dump_mode": parameters["dump_mode"],
+            "execute_query": query_to_line(parameters["execute_query"]),
+        }
+
+        # Add remaining parameters if value is not None
+        for key, value in parameters.items():
+            if value is not None and key not in ["interval"]:
+                parameter_defaults[key] = value
+
+        if "dbt_alias" in parameters:
+            parameter_defaults["dbt_alias"] = parameters["dbt_alias"]
+        if "dataset_id" in parameters:
+            parameter_defaults["dataset_id"] = parameters["dataset_id"]
+        new_interval = parameters["interval"] if "interval" in parameters else interval
+
+        clocks.append(
+            IntervalClock(
+                interval=new_interval,
+                start_date=start_date + timedelta(minutes=runs_interval_minutes * count),
+                labels=labels,
+                parameter_defaults=parameter_defaults,
+            )
+        )
+    return clocks
