b80cdd6fd01d659bbbca19cc3cea4b3d5a24e28f
d116626 <d116626@gmail.com>
2024-02-28 18:13:06 -0300

    fix: config.toml credential

--- prefeitura_rio/pipelines_utils/credentials.py
+++ prefeitura_rio/pipelines_utils/credentials.py
@@ -20,17 +20,17 @@ def secret_to_base64(secret_dict):
 def get_base64_bd_config(projec_id):
     string = f"""# What is the bucket that you are saving all the data? It should be
                 # an unique name.
-                bucket_name = {projec_id}
+                bucket_name = "{projec_id}"
 
                 [gcloud-projects]
 
                 [gcloud-projects.staging]
                     credentials_path = "~/.basedosdados/credentials/staging.json"
-                    name = {projec_id}
+                    name = "{projec_id}"
 
                 [gcloud-projects.prod]
                     credentials_path = "~/.basedosdados/credentials/prod.json"
-                    name = {projec_id}
+                    name = "{projec_id}"
 
                 [api]
                 url = "https://api.dados.rio/api/v1/graphql"
@@ -45,7 +45,7 @@ def get_base64_bd_config(projec_id):
     return encoded_string
 
 
-def base_64_to_string(base_64):
+def base64_to_string(base_64):
     base_64_bytes = base_64.encode("utf-8")
     message_bytes = base64.b64decode(base_64_bytes)
     message = message_bytes.decode("utf-8")
