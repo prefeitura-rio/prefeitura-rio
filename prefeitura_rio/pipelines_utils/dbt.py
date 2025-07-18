12015b86db93a93499a0acf96b489c5de6405d41
Pedro Siqueira <pedro.siqueira@poli.ufrj.br>
2025-07-18 12:41:47 -0300

    fix: Change warn summarize message

--- prefeitura_rio/pipelines_utils/dbt.py
+++ prefeitura_rio/pipelines_utils/dbt.py
@@ -264,7 +264,7 @@ class RunResultSummarizer:
         return f"`{result.node.name}`\n   {result.message}: ``` select * from {result.node.relation_name.replace('`','')}``` \n"  # noqa
 
     def warn(self, result):
-        return f"`{result.node.name}`\n   {result.message}: ``` select * from {result.node.relation_name.replace('`','')}``` \n"  # noqa
+        return f"`{result.node.name}`\n  {result.message.replace('__', '_')} \n"
 
 
 class FreshnessResultSummarizer:
